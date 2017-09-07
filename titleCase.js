6d1673022fb9cbe12b77b754dc45c7415e3ab632
yangyumeng <18026493873@163.com>
2017-09-07 18:21:55 +0800

    Update titleCase.js

--- titleCase.js
+++ titleCase.js
@@ -1,3 +1,4 @@
+//方法一:replace
 function titleCase(str) {
   var arr = str.toLowerCase().split(" ")
   console.log(arr)
@@ -13,3 +14,24 @@ function titleCase(str) {
 }
 
 console.log(titleCase("I'm a little tea pot"));
+
+//方法二:
+//for循环  
+function titleCase(str) {  
+    var i, ss = str.toLowerCase().split(/\s+/);  
+    for (i = 0; i < ss.length; i++) {  
+        ss[i] = ss[i].slice(0, 1).toUpperCase() + ss[i].slice(1);  
+    }  
+    return ss.join(' ');  
+} 
+var a = 'Hi, my name\'s Han Meimei, a SOFTWARE engineer';  
+console.log(titleCase(a));  
+
+//数组+map  
+function titleCase3(s) {  
+    return s.toLowerCase().split(/\s+/).map(function(item, index) {  
+        return item.slice(0, 1).toUpperCase() + item.slice(1);  
+    }).join(' ');  
+}  
+console.log(titleCase3(a));  
+// 思路：根据空白将字符串拆分为数组，对每个单词进行首字母大写处理，并将所有处理后的结果组成一个新数组然后拼接成字符串。
