d029a383c3cbb42db0fb844de15c0e3fecf63aa4
yangyumeng <18026493873@163.com>
2017-09-08 18:41:52 +0800

    Create confirmEnding.js

---
+++ confirmEnding.js
@@ -0,0 +1,15 @@
+//方法:
+function confirmEnding(str, target){
+  var str1 = str.split(" ").pop()
+  var len = target.length
+  if(str1.substr((str1.length-len))===target){
+    return true
+  }
+  return false
+ 
+}
+
+console.log(confirmEnding("Bastian", "n"));
+console.log(confirmEnding("He has to give me a new name", "name")
+)
+console.log(confirmEnding("He has to give me a new name", "me"))
