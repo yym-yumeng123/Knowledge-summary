c20f7393b6b00d98cd5514abd2002345889bfe45
yangyumeng <18026493873@163.com>
2017-09-09 08:58:59 +0800

    Update repeat.js

--- repeat.js
+++ repeat.js
@@ -1,12 +1,15 @@
 //方法一:
 function repeat(str, num) {
+  //如果大于0
   if(num > 0){
     var str1 = str.split(" ")
     var str2 = []
+    //for循环把str1添加到str2的每一个位置
     for(var i = 0;i< num;i++){
      str2[i] = str1
      console.log(str2)
     }
+    //num<0,空字符串
   }else{
     return ""
   }
