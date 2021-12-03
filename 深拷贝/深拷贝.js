7836408a0ee197797f72c5d5fc0669522a981edc
“yym-yumeng123 <18026493873@163.com>
2021-12-03 12:03:27 +0800

    深拷贝 函数 对象 数组

--- 深拷贝/深拷贝.js
+++ 深拷贝/深拷贝.js
@@ -28,6 +28,8 @@ const a1 = JSON.parse(JSON.stringify(a))
  * 不支持 日期: 会变成一个 事件字符串
  * 不支持正则
  * 不支持 Symbol()
+ * 
+ * window.self === window
  */
 const b1 = JSON.parse(JSON.stringify(b))
 
@@ -40,7 +42,54 @@ const type = {
 }
 
 function deepClone(source) {
+  if (source instanceof Object) {
+    if (source instanceof Array) {
+      const dist = new Array()
+      for (const key in source) {
+        dist[key] = deepClone(source[key])
+      }
+      return dist
+    } else if (source instanceof Function) {
+      const dist = function () {
+        return source.apply(this, arguments)
+      }
+      for (const key in source) {
+        dist[key] = deepClone(source[key])
+      }
+    } else {
+      const dist = new Object()
+      for (const key in source) {
+        dist[key] = deepClone(source[key])
+      }
+      return dist
+    }
+  }
+
   return source
 }
 
-deepClone(type)
+// 数组
+const arr = [1, 2, [3, 4], [5, [6]]]
+console.assert(arr[3][0] === deepClone(arr))
+
+// 普通对象断言
+const type = {
+  s: "1",
+  n: 1,
+  b: true,
+  u: undefined,
+  n: null,
+  children: {
+    name: "yym",
+  },
+}
+console.assert(type.s === deepClone(type).s)
+console.assert(type.children !== deepClone(type).children)
+console.assert(type.children.name === deepClone(type).children.name)
+
+// 基本类型断言
+console.assert(1 === deepClone(1))
+console.assert("1" === deepClone("1"))
+console.assert(true === deepClone(true))
+console.assert(null === deepClone(null))
+console.assert(undefined === deepClone(undefined))
