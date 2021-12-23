5e05bdfc829d54e60a18c754cd9f21e769e94aa9
“yym-yumeng123 <18026493873@163.com>
2021-12-23 11:01:38 +0800

    readonly static constructor

--- TypeScript/class2.ts
+++ TypeScript/class2.ts
@@ -85,3 +85,43 @@
     comments: [{ content: "你好", author: "yym" }],
   }
 }
+
+// ! readonly
+{
+  class Axios {
+    readonly site: string = "https://www.google.com"
+  }
+  const instance = new Axios()
+  // instance.site = '23232' 只读属性, 无法赋值
+}
+
+// ! construcotr
+
+{
+  class User {
+    public name: string
+    constructor(name: string) {
+      this.name = this.initName(name)
+    }
+
+    initName(name: string) {
+      return `${name}是个大好人`
+    }
+  }
+  const yym = new User("杨雨萌")
+}
+
+// ! static 静态属性
+{
+  class User {
+    static site: string = "www.example.com"
+    public static getSite(): string {
+      // return this.site 属性“site”在类型“User”上不存在
+      return User.site
+    }
+  }
+  const user = new User()
+  // console.log(user.site); 属性“site”在类型“User”上不存在
+  console.log(User.site, "user.site")
+  console.log(User.getSite(), "User.site")
+}
