0f0f020550eab4d9601a82f7cd39a2cd9a3fc192
yangyumeng <yangyumeng@guanmai.cn>
2022-02-16 19:33:42 +0800

    ruby 迭代器

--- Ruby/菜鸟/范围&迭代器.rb
+++ Ruby/菜鸟/范围&迭代器.rb
@@ -55,3 +55,42 @@ if (1..10) === 5
   puts "5 在(1..10)"
 end
 
+
+
+Ruby 迭代器
+
+迭代(iterate)指的是重复做同样的事, 所以迭代器就是用来重复多次相同的事
+
+迭代器是集合支持的方法, 存储一组数据成员的对象称为集合, 在 Ruby 中, 数组Array 和 哈希 Hash 可以称之为集合
+
+迭代器返回集合的所有元素, 一个接着一个,  我们讨论 each collect
+
+Ruby each 迭代器
+each 迭代器返回数组或哈希的所有元素
+
+collection.each do |variable|
+  code
+end
+
+为集合中每个元素执行code, 在这里, 集合可以是数组的哈希
+
+arr = [1,2,3,4,5]
+arr.each do |i|
+  puts i
+end
+
+Ruby collect 迭代器
+collection = collection.collect
+collect 方法不需要总是与一个块关联, collect方法返回整个集合, 不管他是数组还是哈希
+
+a = [1,2,3,4,5]
+b = Array.new
+b = a.collect{|x|x}
+puts b
+
+注意: collect 方法不是数组间进行复制的正确方式, 这里有另一个称为 clone 的方法，用于复制一个数组到另一个数组
+当您想要对每个值进行一些操作以便获得新的数组时，您通常使用 collect 方法
+
+a = [1,2,3,4,5]
+b = a.collect{|x| 10*x}
+puts b
