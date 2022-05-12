6aa6ff1849a6d09bfb20c7e1affe3380ffb46958
yym-yumeng123 <18026493873@163.com>
2022-05-12 15:31:50 +0800

    去掉冗余代码 & 添加 normal.css

--- icss/src/main.tsx
+++ icss/src/main.tsx
@@ -1,10 +1,11 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import "../node_modules/normalize.css";
+import "./index.scss";
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
+ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
-)
+);
