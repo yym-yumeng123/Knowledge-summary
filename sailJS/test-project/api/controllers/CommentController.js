bf37add109ffd930584054e08e1868bc964128aa
yym-yumeng123 <18026493873@163.com>
2022-08-16 18:03:54 +0800

    摸索

---
+++ sailJS/test-project/api/controllers/CommentController.js
@@ -0,0 +1,48 @@
+/**
+ * CommentController
+ *
+ * @description :: Server-side actions for handling incoming requests.
+ * @help        :: See https://sailsjs.com/docs/concepts/actions
+ */
+
+module.exports = {
+  
+
+  /**
+   * `CommentController.create()`
+   */
+  create: async function (req, res) {
+    return res.json({
+      todo: 'create() is not implemented yet!'
+    });
+  },
+
+  /**
+   * `CommentController.destroy()`
+   */
+  destroy: async function (req, res) {
+    return res.json({
+      todo: 'destroy() is not implemented yet!'
+    });
+  },
+
+  /**
+   * `CommentController.tag()`
+   */
+  tag: async function (req, res) {
+    return res.json({
+      todo: 'tag() is not implemented yet!'
+    });
+  },
+
+  /**
+   * `CommentController.like()`
+   */
+  like: async function (req, res) {
+    return res.json({
+      todo: 'like() is not implemented yet!'
+    });
+  }
+
+};
+
