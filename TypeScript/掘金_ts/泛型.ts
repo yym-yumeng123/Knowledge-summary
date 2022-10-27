74e7fc90a4938840b72b3653ceb0d23065bc4011
yym-yumeng123 <18026493873@163.com>
2022-10-27 11:51:33 +0800

    泛型约束和默认值

--- TypeScript/掘金_ts/泛型.ts
+++ TypeScript/掘金_ts/泛型.ts
@@ -29,3 +29,37 @@ type IsEqual<T> = T extends true ? 1 : 2;
 type A = IsEqual<true>; // 1
 type B = IsEqual<false>; // 2
 type C = IsEqual<"yym">; // 2
+
+{
+  type Factory<T = boolean> = T | number | string;
+  const foo: Factory = false;
+
+  function Factory1(typeArg = boolean) {
+    return [typeArg, "number", "string"];
+  }
+
+  function add(source: number, add: number) {
+    if (typeof source !== "number" || typeof add !== "number") {
+      throw new Error("Invalid arguments");
+    }
+
+    return source + add;
+  }
+
+  type ResStatus<ResCode extends number> = ResCode extends 10000 | 10001 | 10002
+    ? "success"
+    : "failure";
+
+  type Res1 = ResStatus<10000>; // 'success'
+  type Res2 = ResStatus<20000>; // 'success'
+  type Res3 = ResStatus<"10000">; // 类型“string”不满足约束“number
+
+  type ResStatus1<ResCode extends number = 10000> = ResCode extends
+    | 10000
+    | 10001
+    | 10002
+    ? "success"
+    : "failure";
+
+  type Res4 = ResStatus1;
+}
