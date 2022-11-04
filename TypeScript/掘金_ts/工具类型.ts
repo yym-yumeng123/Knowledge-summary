1b027b26c78fe65514a9601459702d3df67673b5
yym-yumeng123 <18026493873@163.com>
2022-11-04 11:07:58 +0800

    集合工具和模式匹配工具

--- TypeScript/掘金_ts/工具类型.ts
+++ TypeScript/掘金_ts/工具类型.ts
@@ -129,3 +129,84 @@ type Tmp101 = Exclude<1, 2>; // 1
 type Tmp102 = Exclude<1 | 2, 2>; // 1
 type Tmp103 = Exclude<1 | 2 | 3, 2 | 3>; // 1
 type Tmp104 = Exclude<1 | 2 | 3, 2 | 4>; // 1 | 3
+
+type MyExtract<T, U> = T extends U ? T : never;
+type MyExclude<T, U> = T extends U ? never : T;
+
+type AExtractB = Extract<1 | 2 | 3, 1 | 2 | 4>; // 1 | 2
+
+type _AExtractB =
+  | (1 extends 1 | 2 | 4 ? 1 : never) // 1
+  | (2 extends 1 | 2 | 4 ? 2 : never) // 2
+  | (3 extends 1 | 2 | 4 ? 3 : never); // never
+
+// 并集
+export type Concurrence<A, B> = A | B;
+// 交集
+type Intersection<A, B> = A extends B ? A : never;
+// 差集
+type Difference<A, B> = A extends B ? never : A;
+// 补集
+type Complement<A, B extends A> = Difference<A, B>;
+
+type NonNullable<T> = T extends null | undefined ? never : T;
+type _NonNullable<T> = Difference<T, null | undefined>;
+
+type FunctionType = (...args: any) => any;
+type Parameters<T extends FunctionType> = T extends (...args: infer P) => any
+  ? P
+  : never;
+
+type ReturnType<T extends FunctionType> = T extends (...args: any) => infer R
+  ? R
+  : any;
+
+type para = Parameters<(...arg: string[]) => string>; // string[]
+type return1 = ReturnType<(...arg: string[]) => string>; // string
+
+type FirstParameter<T extends FunctionType> = T extends (
+  arg: infer F,
+  ...args: any
+) => any
+  ? F
+  : never;
+
+type GetLast<T extends unknown[]> = T extends [...any, infer L] ? L : T[number];
+type FuncLastArg<T> = T extends (...args: infer G) => unknown ? GetLast<G> : T;
+
+type FuncFoo = (arg: number) => void;
+type FuncBar = (...args: string[]) => void;
+
+type FooFirstParameter = FirstParameter<FuncFoo>; // number
+type BarFirstParameter = FirstParameter<FuncBar>; // string
+
+type ClassType = abstract new (...args: any) => any;
+
+type ConstructorParameters<T extends ClassType> = T extends abstract new (
+  ...args: infer P
+) => any
+  ? P
+  : never;
+
+type InstanceType<T extends ClassType> = T extends abstract new (
+  ...args: any
+) => infer R
+  ? R
+  : any;
+
+type FirstArrayItemType<T extends any[]> = T extends [infer F, ...any[]]
+  ? F extends string
+    ? F
+    : never
+  : never;
+
+type Tmp1 = FirstArrayItemType<[599, "linbudu"]>; // never
+type Tmp2 = FirstArrayItemType<["linbudu", 599]>; // 'linbudu'
+type Tmp3 = FirstArrayItemType<["linbudu"]>; // 'linbudu'
+
+type FirstArrayItemType1<T extends any[]> = T extends [
+  infer P extends string,
+  ...any[]
+]
+  ? P
+  : never;
