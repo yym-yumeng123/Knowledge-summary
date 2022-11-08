a9d0e9b2896ebcfe2c03d98be3289a8d2f9af3a2
yym-yumeng123 <18026493873@163.com>
2022-11-08 18:02:08 +0800

    模式匹配工具

--- TypeScript/掘金_ts/类型编程进阶.ts
+++ TypeScript/掘金_ts/类型编程进阶.ts
@@ -328,3 +328,46 @@ type ObjectComplement<T extends U, U extends PlainObjectType> = Pick<
   T,
   ObjectKeysComplement<T, U>
 >;
+
+type FirstParameter<T extends FunctionStruct> = T extends (
+  arg: infer P,
+  ...args: any
+) => any
+  ? P
+  : never;
+
+type FunctionType = (...args: any) => any;
+type LastParameter<T extends FunctionType> = T extends (arg: infer P) => any
+  ? P
+  : T extends (...args: infer R) => any
+  ? R extends [...any, infer Q]
+    ? Q
+    : never
+  : never;
+
+type FuncFoo = (arg: number) => void;
+type FuncBar = (...args: string[]) => void;
+type FuncBaz = (arg1: string, arg2: boolean) => void;
+
+type FooLastParameter = LastParameter<FuncFoo>; // number
+type BarLastParameter = LastParameter<FuncBar>; // string
+type BazLastParameter = LastParameter<FuncBaz>; // boolean
+
+type Awaited<T> = T extends null | undefined
+  ? T
+  : T extends object & { then(onfulfilled: infer F): any }
+  ? F extends (value: infer V, ...args: any) => any
+    ? Awaited<V>
+    : never
+  : T;
+
+type Tmp11 = {} extends { prop: number } ? "Y" : "N"; // "N"
+type Tmp21 = {} extends { prop?: number } ? "Y" : "N"; // "Y"
+
+type RequiredKeys<T> = {
+  [K in keyof T]-?: {} extends Pick<T, K> ? never : K;
+};
+
+type OptionalKeys<T> = {
+  [K in keyof T]-?: {} extends Pick<T, K> ? K : never;
+}[keyof T];
