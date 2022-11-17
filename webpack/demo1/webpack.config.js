f98d5b53ad4e71699b9299183fcae240954db5d3
yym-yumeng123 <18026493873@163.com>
2022-11-18 00:23:12 +0800

    代码分离

--- webpack/demo1/webpack.config.js
+++ webpack/demo1/webpack.config.js
@@ -1,22 +1,29 @@
 const path = require("path")
-const HtmlWebpackPlugins = require('html-webpack-plugin')
+const HtmlWebpackPlugins = require("html-webpack-plugin")
 
 module.exports = {
   mode: "development",
   entry: {
-    index: './src/index.js',
-    print: './src/print.js'
+    index: {
+      import: "./src/index.js",
+      dependOn: "shared",
+    },
+    another: {
+      import: "./src/another-module.js",
+      dependOn: "shared",
+    },
+    shared: "lodash",
   },
   output: {
     filename: "[name].bundle.js",
     path: path.resolve(__dirname, "dist"),
-    clean: true
+    clean: true,
   },
-  devtool: 'inline-source-map',
+  devtool: "inline-source-map",
   plugins: [
     new HtmlWebpackPlugins({
-      title: 'Development'
-    })
+      title: "Development",
+    }),
   ],
   // module: {
   //   rules: [
@@ -64,9 +71,12 @@ module.exports = {
   //   ],
   // },
   devServer: {
-    static: './dist'
+    static: "./dist",
   },
   optimization: {
-    runtimeChunk: 'single',
+    runtimeChunk: "single",
+    splitChunks: {
+      chunks: "all",
+    },
   },
 }
