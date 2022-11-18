fd8cc79d43902f32f8ffd02bffc94b08799098b2
yym-yumeng123 <18026493873@163.com>
2022-11-18 13:31:00 +0800

    环境变量

--- webpack/webpack-numbers/webpack.config.js
+++ webpack/webpack-numbers/webpack.config.js
@@ -1,21 +1,28 @@
 const path = require("path")
 
-module.exports = {
-  entry: "./src/index.js",
-  output: {
-    path: path.resolve(__dirname, "dist"),
-    filename: "webpack-number.js",
-    library: {
-      name: "webpackNumbers",
-      type: "umd",
+module.exports = (env) => {
+  // Use env.<YOUR VARIABLE> here:
+  console.log("Goal: ", env.goal) // 'local'
+  console.log("Production: ", env.production) // true
+  return {
+    entry: "./src/index.js",
+    output: {
+      path: path.resolve(__dirname, "dist"),
+      filename: "webpack-number.js",
+      library: {
+        name: "webpackNumbers",
+        type: "umd",
+      },
     },
-  },
-  externals: {
-    lodash: {
-      commonjs: "lodash",
-      commonjs2: "lodash",
-      amd: "lodash",
-      root: "_",
+    externals: {
+      lodash: {
+        commonjs: "lodash",
+        commonjs2: "lodash",
+        amd: "lodash",
+        root: "_",
+      },
     },
-  },
+  }
 }
+
+// npx webpack --env goal=local --env production --progress
