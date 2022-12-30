5037d7b9a4bdd1848123de055144bc73fa8d7b6b
yym-yumeng123 <18026493873@163.com>
2022-12-30 22:20:11 +0800

    类型模板字符串

--- TypeScript/掘金_ts/模板字符串工具类型进阶.ts
+++ TypeScript/掘金_ts/模板字符串工具类型进阶.ts
@@ -37,3 +37,65 @@ type StartsWithRes2 = StartsWith<"linbudu", ""> // true
 type StartsWithRes3 = StartsWith<"linbudu", " "> // false
 type StartsWithRes4 = StartsWith<"", ""> // true
 type StartsWithRes5 = StartsWith<" ", ""> // true
+
+/**
+ * @param str
+ * @param 需要替换的
+ * @param 替换为
+ */
+export type Replace<
+  Str extends string,
+  Search extends string,
+  Replacement extends string
+> = Str extends `${infer Head}${Search}${infer Tail}`
+  ? `${Head}${Replacement}${Tail}`
+  : Str
+
+type ReplaceRes1 = Replace<"杨雨萌", "杨", "我是好人"> // "我是好人雨萌"
+type ReplaceRes2 = Replace<"杨雨萌", "?", "??">
+
+export type ReplaceAll<
+  Str extends string,
+  Search extends string,
+  Replacement extends string
+> = Str extends `${infer Head}${Search}${infer Tail}`
+  ? ReplaceAll<`${Head}${Replacement}${Tail}`, Search, Replacement>
+  : Str
+
+// "mmm.linbudu.top"
+type ReplaceAllRes1 = ReplaceAll<"www.linbudu.top", "w", "m">
+// "www-linbudu-top"
+type ReplaceAllRes2 = ReplaceAll<"www.linbudu.top", ".", "-">
+
+export type Split<Str extends string> =
+  Str extends `${infer Head}-${infer Body}-${infer Tail}`
+    ? [Head, Body, Tail]
+    : []
+type SplitRes1 = Split<"lin-bu-du"> // ["lin", "bu", "du"]
+
+export type Split1<
+  Str extends string,
+  Delimiter extends string
+> = Str extends `${infer Head}${Delimiter}${infer Tail}`
+  ? [Head, ...Split1<Tail, Delimiter>]
+  : Str extends Delimiter
+  ? []
+  : [Str]
+
+type SplitRes11 = Split1<"linbudu,599,fe", ",">
+type SplitRes13 = Split1<"linbudu", "">
+
+export type Join<
+  List extends Array<string | number>,
+  Delimiter extends string
+> = List extends []
+  ? ""
+  : List extends [string | number]
+  ? `${List[0]}`
+  : List extends [string | number, ...infer Rest]
+  ? // @ts-expect-error
+    `${List[0]}${Delimiter}${Join<Rest, Delimiter>}`
+  : string
+
+// "lin-bu-du"
+type JoinRes3 = Join<["lin", "bu", "du"], "-">
