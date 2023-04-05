e94e674f48d3132ab2b7011954f993a9021d8c45
yym-yumeng123 <18026493873@163.com>
2023-04-05 23:47:14 +0800

    symbol 类型

--- 杂记/高级程序设计4.js
+++ 杂记/高级程序设计4.js
@@ -45,3 +45,32 @@ let taggedResult = simpleTag`${a} + ${b} = ${a + b}`
 // 15
 console.log(untaggedResult) // "6 + 9 = 15"
 console.log(taggedResult) // "foobar"
+
+let sym = Symbol()
+console.log(typeof sym)
+
+let fooGlobalSymbol = Symbol.for("foo")
+console.log(typeof fooGlobalSymbol) // symbol
+
+let s1 = Symbol("foo"),
+  s2 = Symbol("bar"),
+  s3 = Symbol("baz"),
+  s4 = Symbol("qux")
+let o = {
+  [s1]: "foo val",
+}
+// 这样也可以：o[s1] = 'foo val';
+console.log(o)
+// {Symbol(foo): foo val}
+Object.defineProperty(o, s2, { value: "bar val" })
+console.log(o)
+// {Symbol(foo): foo val, Symbol(bar): bar val}
+Object.defineProperties(o, {
+  [s3]: { value: "baz val" },
+  [s4]: { value: "qux val" },
+})
+console.log(o)
+// {Symbol(foo): foo val, Symbol(bar): bar val,
+// Symbol(baz): baz val, Symbol(qux): qux val}
+
+Reflect.ownKeys()
