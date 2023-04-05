b6dc02aa2bbae22b624fbc3aa65823c864690788
yym-yumeng123 <18026493873@163.com>
2023-04-05 23:27:35 +0800

    String 类型

--- 杂记/高级程序设计4.js
+++ 杂记/高级程序设计4.js
@@ -27,3 +27,21 @@ if (true) {
   console.log(age) // 26
 }
 console.log(age) // ReferenceError: age 没有定义
+
+let a = 6
+let b = 9
+function simpleTag(strings, aValExpression, bValExpression, sumExpression) {
+  console.log(strings)
+  console.log(aValExpression)
+  console.log(bValExpression)
+  console.log(sumExpression)
+  return "foobar"
+}
+let untaggedResult = `${a} + ${b} = ${a + b}`
+let taggedResult = simpleTag`${a} + ${b} = ${a + b}`
+// ["", " + ", " = ", ""]
+// 6
+// 9
+// 15
+console.log(untaggedResult) // "6 + 9 = 15"
+console.log(taggedResult) // "foobar"
