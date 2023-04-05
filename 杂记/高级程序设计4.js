b46d03b580d3b6746c1aa1f4614a3675d851b1eb
yym-yumeng123 <18026493873@163.com>
2023-04-05 15:09:13 +0800

    var let const

--- 杂记/高级程序设计4.js
+++ 杂记/高级程序设计4.js
@@ -1,3 +1,29 @@
-let script = document.createElement('script')
-script.src = 'demo.js'
+let script = document.createElement("script")
+script.src = "demo.js"
 document.head.appendChild(script)
+
+var message // undefined
+var message = "hi"
+message = 100 // 不推荐改变变量保存值的类型
+
+function test() {
+  var message = "hi" // 局部变量
+}
+test()
+console.log(message) // 出错
+
+var message = "hi",
+  found = false,
+  age = 29
+
+if (true) {
+  var name = "matt"
+  console.log("name", name)
+}
+console.log("name", name)
+
+if (true) {
+  let age = 26
+  console.log(age) // 26
+}
+console.log(age) // ReferenceError: age 没有定义
