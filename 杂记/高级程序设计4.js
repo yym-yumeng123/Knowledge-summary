d9116d7fc7d9d0f43ff70062972c8e975acb38d2
yym-yumeng123 <18026493873@163.com>
2023-04-06 16:18:04 +0800

    执行上下文和作用域

--- 杂记/高级程序设计4.js
+++ 杂记/高级程序设计4.js
@@ -153,3 +153,29 @@ function setName(obj) {
 let person = new Object()
 setName(person)
 console.log(person.name) // "Nicholas"
+
+var color = "blue"
+
+function changeColor() {
+  if (color === "blue") {
+    color = "red"
+  } else {
+    color = "blue"
+  }
+}
+changeColor()
+
+var color = "blue"
+function changeColor() {
+  let anotherColor = "red"
+  function swapColors() {
+    let tempColor = anotherColor
+    anotherColor = color
+    color = tempColor
+    // 这里可以访问 color、anotherColor 和 tempColor
+  }
+  // 这里可以访问 color 和 anotherColor，但访问不到 tempColor
+  swapColors()
+}
+// 这里只能访问 color
+changeColor()
