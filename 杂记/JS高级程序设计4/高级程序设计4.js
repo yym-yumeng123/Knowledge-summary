a061bf89183a1bec9b4a102d063e005bcdba806c
yym-yumeng123 <18026493873@163.com>
2023-08-12 19:57:18 +0800

    原型式/寄生式/寄生组合式继承

--- 杂记/JS高级程序设计4/高级程序设计4.js
+++ 杂记/JS高级程序设计4/高级程序设计4.js
@@ -572,17 +572,61 @@ console.log("instance.getSuperValue()", instance.getSuperValue())
 
 function SuperType(name) {
   this.name = name
+  this.colors = ["red", "blue", "green"]
 }
-
-function SubType() {
-  // 继承 SuperType 并传参
-  SuperType.call(this, 'yym')
-  // 实例属性
-  this.age = 18
+SuperType.prototype.sayName = function () {
+  console.log(this.name)
 }
-
-let instance1 = new SubType()
+function SubType(name, age) {
+  // 继承属性
+  SuperType.call(this, name)
+  this.age = age
+}
+// 继承方法
+SubType.prototype = new SuperType()
+SubType.prototype.sayAge = function () {
+  console.log(this.age)
+}
+let instance1 = new SubType("Nicholas", 29)
 instance1.colors.push("black")
 console.log(instance1.colors) // "red,blue,green,black"
-let instance2 = new SubType()
+instance1.sayName() // "Nicholas";
+instance1.sayAge() // 29
+let instance2 = new SubType("Greg", 27)
 console.log(instance2.colors) // "red,blue,green"
+instance2.sayName() // "Greg";
+instance2.sayAge() // 27
+
+function object(o) {
+  function F() {}
+  F.prototype = o
+  return new F()
+}
+
+let person = {
+  name: "Nicholas",
+  friends: ["Shelby", "Court", "Van"],
+}
+let anotherPerson = Object.cteate(person)
+anotherPerson.name = "Greg"
+anotherPerson.friends.push("Rob")
+let yetAnotherPerson = Object.create(person)
+yetAnotherPerson.name = "Linda"
+yetAnotherPerson.friends.push("Barbie")
+console.log(person.friends) // "Shelby,Court,Van,Rob,Barbie"
+
+function createAnother(original) {
+  let clone = object(original)
+  clone.sayHi = function () {
+    // 以某种方式增强这个对象
+    console.log("hi")
+  }
+  return clone
+}
+
+let person = {
+  name: "Nicholas",
+  friends: ["Shelby", "Court", "Van"],
+}
+let anotherPerson = createAnother(person)
+anotherPerson.sayHi() // "hi
