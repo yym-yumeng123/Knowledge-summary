eba65ca00e5c91e4b193bf10bf6126a01e8fcf49
yym-yumeng123 <18026493873@163.com>
2023-08-18 12:08:26 +0800

    理解参数没有重载

--- 杂记/JS高级程序设计4/函数10.js
+++ 杂记/JS高级程序设计4/函数10.js
@@ -5,3 +5,27 @@ ints.map(function (i) {
 })
 
 ints.map((i) => i + 1)
+
+function foo() {}
+console.log("foo.bind(null)", foo.bind(null).name)
+
+let dog = {
+  get age() {
+    return this.year
+  },
+  set age(newAge) {
+    this.year = newAge
+  },
+}
+
+let propertyDescriptor = Object.getOwnPropertyDescriptor(dog, "age")
+console.log(propertyDescriptor.get.name) // get age
+console.log(propertyDescriptor.set.name) // set age
+
+function doAdd(num1, num2) {
+  if (arguments.length === 1) {
+    console.log(num1 + 10)
+  } else if (arguments.length === 2) {
+    console.log(arguments[0] + num2)
+  }
+}
